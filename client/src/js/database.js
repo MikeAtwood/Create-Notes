149a6f5d5af6e056284214c8b557db6d5c295f9f
Deadpool1 <atwood169@yahoo.com>
2022-09-07 18:57:00 -0500

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,6 +22,13 @@ export const putDb = async (content) => {
   console.log("Data saved to the database", result);
 };
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get({ jate: content });
+  const result = await request;
+  console.log(result);
+} 
 
 initdb();
